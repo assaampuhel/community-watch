1892fad843deb99d3e1756163f8970c62f7e29ae
shouryasharma22 <shouryasharma1203@gmail.com>
2026-05-12 16:05:52 +0530

    Added Cheater DB component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import AuthForm from "./components/Authform";
 import HomePage from "./components/HomePage";
 import ActiveReports from "./components/ActiveReports";
-
+import CheaterDB from "./components/CheaterDB";
 function App() {
   return (
     <div>
       {/* <HomePage/> */}
       <AuthForm />
       {/* <ActiveReports /> */}
+      <CheaterDB />
     </div>
   )
 }
